dc7b82eb0b1d6a504619ec6b946aa9e247eb0cd0
Imre Gera <hanziness@windowslive.com>
2026-02-13 21:33:00 +0100

    feat: Add keyboard handler

--- packages/framework/controls.ts
+++ packages/framework/controls.ts
@@ -1,2 +1,3 @@
+export { default as KeyboardHandler } from "./src/dev/KeyboardHandler.astro";
 export { default as Next } from "./src/dev/Next.astro";
 export { default as SlideControls } from "./src/dev/SlideControls.astro";
