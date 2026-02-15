1c5dfb031ff59e17e5d4b37af36d2e0255fdae62
Imre Gera <hanziness@windowslive.com>
2026-02-15 15:53:23 +0100

    feat(framework): Export `Step`

--- packages/framework/index.ts
+++ packages/framework/index.ts
@@ -3,6 +3,7 @@ export { default as DebugCurrentSection } from "./src/dev/CurrentSection.astro";
 export { default as Root } from "./src/Root.astro";
 export { default as Slide } from "./src/Slide.astro";
 export { default as Slideshow } from "./src/Slideshow.astro";
+export { default as Step } from "./src/Step.astro";
 export { default as StyleHelper } from "./src/StyleHelper.astro";
 export type { SectionInfo } from "./src/store";
 // Section utilities
