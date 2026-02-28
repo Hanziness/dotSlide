859739e31dcfd24b5d84decd3dc3d873cc5e1191
Imre Gera <hanziness@windowslive.com>
2026-02-28 17:54:57 +0100

    refactor(framework): Update exports

--- packages/framework/widgets.ts
+++ packages/framework/widgets.ts
@@ -1,2 +1,4 @@
 export { default as CurrentSection } from "./src/widgets/CurrentSection.astro";
+export { default as CurrentSlide } from "./src/widgets/CurrentSlide.astro";
 export { default as Progress } from "./src/widgets/Progress.astro";
+export { default as TotalSlides } from "./src/widgets/TotalSlides.astro";
