b1281351cfb30aeec470b4bb0fbafcd0585d6ed2
Imre Gera <imre.gera@volumental.com>
2026-03-07 18:01:39 +0100

    feat(framework): Export Overlay and context helpers

--- packages/framework/index.ts
+++ packages/framework/index.ts
@@ -1,4 +1,5 @@
 export { default as Image } from "./src/Image.astro";
+export { default as Overlay } from "./src/overlay/Overlay.astro";
 export { default as Root } from "./src/Root.astro";
 export { default as Section } from "./src/Section.astro";
 export { default as Slide } from "./src/Slide.astro";
@@ -6,6 +7,11 @@ export { default as Slideshow } from "./src/Slideshow.astro";
 export { default as Step } from "./src/Step.astro";
 export { default as StyleHelper } from "./src/StyleHelper.astro";
 export type { SectionInfo } from "./src/store";
+
+export { useSlideContext } from './src/store/context/slide'
+export { useSlideshowContext } from './src/store/context/slideshow'
+
+
 // Section utilities
 export {
   buildSectionHierarchy,
