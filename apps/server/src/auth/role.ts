0578987faa306b46eb3bb41ebac504b6798feaf8
Imre Gera <hanziness@windowslive.com>
2026-03-12 21:10:06 +0100

    feat(server): Separate `setSessionRole`

---
+++ apps/server/src/auth/role.ts
@@ -0,0 +1,4 @@
+export enum SessionRole {
+    Viewer = "viewer",
+    Presenter = "presenter"
+}
--- apps/server/src/session.ts
+++ apps/server/src/session.ts
@@ -1,8 +1,19 @@
+import { eq } from "drizzle-orm";
 import { auth } from "./auth";
+import type { SessionRole } from "./auth/role";
+import { db } from "./db";
+import { session as sessionTable } from "./db/auth";
 
 const freshSessionQuery = {
   disableCookieCache: true,
 } as const;
+
+export async function setSessionRole(id: string, role: SessionRole) {
+  return await db
+    .update(sessionTable)
+    .set({ presentationRole: role })
+    .where(eq(sessionTable.id, id));
+}
 
 export async function getFreshSession(
   headers: Headers,
