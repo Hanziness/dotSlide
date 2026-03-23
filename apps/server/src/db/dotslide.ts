9937caed124ea8a146a4a6685f0e4ac4466f8583
Imre Gera <hanziness@windowslive.com>
2026-03-23 21:05:46 +0100

    feat(server): Add members and invites to database

--- apps/server/src/db/dotslide.ts
+++ apps/server/src/db/dotslide.ts
@@ -3,6 +3,7 @@ import {
   blob,
   index,
   integer,
+  primaryKey,
   sqliteTable,
   text,
 } from "drizzle-orm/sqlite-core";
@@ -10,12 +11,37 @@ import { user } from "./auth";
 
 export const presentation = sqliteTable("presentation", {
   id: text("id").primaryKey(),
-  state: text("state", { mode: "json" }),
-  owner: text("owner").references(() => user.id),
+  state: text("state", { mode: "json" })
 });
 
-export const presentationRelations = relations(presentation, ({ one }) => ({
-  owner: one(user)
+export const members = sqliteTable("members", {
+  presentation: text("id").references(() => presentation.id, { onDelete: "cascade" }).notNull(),
+  user: text("user").references(() => user.id, { onDelete: "cascade" }).notNull(),
+  role: text("role", { enum: ["presenter", "controller"] }).notNull(),
+  createdAt: integer("created_at", { mode: "timestamp_ms" }).$defaultFn(() => new Date()),
+}, (t) => [
+  primaryKey({ columns: [t.presentation, t.user] }),
+  index("membersIdx").on(t.user, t.presentation)
+])
+
+export const membersRelations = relations(members, ({ one }) => ({
+  presentation: one(presentation),
+  user: one(user)
+}))
+
+export const invites = sqliteTable("invites", {
+  id: text("id").primaryKey(),
+  presentation: text("presentation").references(() => presentation.id, { onDelete: "cascade" }).notNull(),
+  issuedBy: text("issued_by").references(() => user.id, { onDelete: "cascade" }).notNull(),
+  expiresAt: integer("expires_at", { mode: "timestamp_ms" }).$defaultFn(() => new Date(Date.now() + 300 * 1000)),
+  redeemedAt: integer("redeemed_at", { mode: "timestamp_ms" })
+}, (t) => [
+  index("invites_presentation_idx").on(t.presentation)
+])
+
+export const invitesRelations = relations(invites, ({ one }) => ({
+  presentation: one(presentation),
+  issuedBy: one(user)
 }))
 
 export const thumbnails = sqliteTable("thumbnails", {
