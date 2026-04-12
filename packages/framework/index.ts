1d97de19838b4b0d3b0b5f00298978b3cae30a42
Imre Gera <hanziness@windowslive.com>
2026-04-12 23:59:52 +0200

    feat(framework): Add Counter component

--- packages/framework/index.ts
+++ packages/framework/index.ts
@@ -1,3 +1,4 @@
+export { default as Counter } from "./src/Counter.astro";
 export { default as Image } from "./src/Image.astro";
 export { default as Overlay } from "./src/overlay/Overlay.astro";
 export { default as Root } from "./src/Root.astro";
@@ -7,6 +8,7 @@ export { default as Slideshow } from "./src/Slideshow.astro";
 export { default as Step } from "./src/Step.astro";
 export { default as StyleHelper } from "./src/StyleHelper.astro";
 export type { SectionInfo } from "./src/store";
+export type { CounterInfo } from "./src/store/context/slideshow";
 export { useSlideContext } from "./src/store/context/slide";
 export { useSlideshowContext } from "./src/store/context/slideshow";
 // Section utilities
