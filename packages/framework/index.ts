34cc66146236b7694827896c5a1dc90eab375cdb
Imre Gera <hanziness@windowslive.com>
2026-04-12 16:28:04 +0200

    feat(framework): Add Video component

--- packages/framework/index.ts
+++ packages/framework/index.ts
@@ -7,11 +7,8 @@ export { default as Slideshow } from "./src/Slideshow.astro";
 export { default as Step } from "./src/Step.astro";
 export { default as StyleHelper } from "./src/StyleHelper.astro";
 export type { SectionInfo } from "./src/store";
-
-export { useSlideContext } from './src/store/context/slide'
-export { useSlideshowContext } from './src/store/context/slideshow'
-
-
+export { useSlideContext } from "./src/store/context/slide";
+export { useSlideshowContext } from "./src/store/context/slideshow";
 // Section utilities
 export {
   buildSectionHierarchy,
@@ -20,3 +17,4 @@ export {
   getSlidePositionInSection,
   type SlidePosition,
 } from "./src/utils/section";
+export { default as Video } from "./src/Video.astro";
