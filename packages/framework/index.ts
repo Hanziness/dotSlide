3cd8c2f3c547df1710435e1073e692145690aad7
Imre Gera <hanziness@windowslive.com>
2026-04-15 21:20:45 +0200

    feat(framework): Add Reference component

--- packages/framework/index.ts
+++ packages/framework/index.ts
@@ -1,6 +1,7 @@
 export { default as Counter } from "./src/Counter.astro";
 export { default as Image } from "./src/Image.astro";
 export { default as Overlay } from "./src/overlay/Overlay.astro";
+export { default as Reference } from "./src/Reference.astro";
 export { default as Root } from "./src/Root.astro";
 export { default as Section } from "./src/Section.astro";
 export { default as Slide } from "./src/Slide.astro";
@@ -8,8 +9,8 @@ export { default as Slideshow } from "./src/Slideshow.astro";
 export { default as Step } from "./src/Step.astro";
 export { default as StyleHelper } from "./src/StyleHelper.astro";
 export type { SectionInfo } from "./src/store";
-export type { CounterInfo } from "./src/store/context/slideshow";
 export { useSlideContext } from "./src/store/context/slide";
+export type { CounterInfo } from "./src/store/context/slideshow";
 export { useSlideshowContext } from "./src/store/context/slideshow";
 // Section utilities
 export {
