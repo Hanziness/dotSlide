3b0a697090d3e4061f41c0cd89c60fd9d2755dee
Imre Gera <hanziness@windowslive.com>
2026-04-15 19:50:52 +0200

    refactor: Rename Row to Flex

--- packages/framework/layout.ts
+++ packages/framework/layout.ts
@@ -1,5 +1,5 @@
 export { default as Item } from "./src/layout/Item.astro";
-export { default as Row } from "./src/layout/Row.astro";
+export { default as Flex } from "./src/layout/Flex.astro";
 
 import { default as ListRoot } from "./src/layout/List.astro";
 import { default as ListItem } from "./src/layout/ListItem.astro";
