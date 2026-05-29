3d90b945ebc7568066c9f1b22342679354aee532
Imre Gera <hanziness@windowslive.com>
2026-05-29 21:21:54 +0200

    refactor(remote): Create auth client locally

--- packages/remote/src/rpc-client.ts
+++ packages/remote/src/rpc-client.ts
@@ -1,7 +1,15 @@
-import { dsClient } from "@dotslide/server/client";
+import { createAuthClientInstance, dsClient } from "@dotslide/server/client";
 
-export const client = dsClient("http://localhost:9876", {
+// Derive server host from the browser's current hostname (same site, different port)
+const serverUrl =
+  typeof window !== "undefined"
+    ? `http://${window.location.hostname}:9876`
+    : "http://localhost:9876";
+
+export const client = dsClient(serverUrl, {
   init: {
     credentials: "include",
   },
 });
+
+export const authClient = createAuthClientInstance(serverUrl)
