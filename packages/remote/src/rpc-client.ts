e49db884cd06ec3c4c8ed9fd9a9304444c270da7
Imre Gera <hanziness@windowslive.com>
2026-05-29 19:29:25 +0200

    fix(remote): Fix formatting

--- packages/remote/src/rpc-client.ts
+++ packages/remote/src/rpc-client.ts
@@ -1,7 +1,7 @@
 import { dsClient } from "@dotslide/server/client";
 
-export const client = dsClient('http://localhost:9876', {
-    init: {
-        credentials: "include"
-    }
-})
+export const client = dsClient("http://localhost:9876", {
+  init: {
+    credentials: "include",
+  },
+});
